ea43d6f80a63db7772e1bde5e44ce87e07df7252
David Medin <david@davidmedin.com>
2022-11-25 17:52:28 -0600

    created a render of a tile stack

---
+++ Assets/scripts/Stacker.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stacker : MonoBehaviour
+{
+    private GameObject[] stack;
+
+    [SerializeField] private GameObject hex_cell;
+
+    [SerializeField] private RenderTexture target_texture;
+
+    [SerializeField] private int _render_size;
+
+    [SerializeField] private GameObject ui_target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target_texture.width = (int)Math.Ceiling(_render_size * 0.3);
+        target_texture.height = _render_size;
+        
+        // Do the ui math for me. hahaha
+        ui_target.GetComponent<RawImage>().SetNativeSize();
+        
+        stack = new GameObject[5];
+        // Create new objects to stack
+        for (int i = 0; i < 5; i++)
+        {
+            stack[i] = Instantiate(hex_cell,transform.position + new Vector3(0,i*1.25f, 3), Quaternion.Euler(-90+150,0,0));
+            stack[i].transform.parent = transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
