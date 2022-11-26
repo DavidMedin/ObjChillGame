cbe572931bf3f28a8ef1f2068afe1078cd71cfec
David Medin <david@davidmedin.com>
2022-11-26 11:34:00 -0600

    Placing tiles now removes from the stack

--- Assets/scripts/Stacker.cs
+++ Assets/scripts/Stacker.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class Stacker : MonoBehaviour
 {
     private GameObject[] stack;
-
+    private int last = 5;
     [SerializeField] private GameObject hex_cell;
 
     [SerializeField] private RenderTexture target_texture;
@@ -19,7 +21,6 @@ public class Stacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int layer = LayerMask.NameToLayer("stack");
         
         target_texture.width = (int)Math.Ceiling(_render_size * 0.3);
         target_texture.height = _render_size;
@@ -28,18 +29,67 @@ public class Stacker : MonoBehaviour
         ui_target.GetComponent<RawImage>().SetNativeSize();
         
         stack = new GameObject[5];
+
+        Repopulate();
+    }
+    
+    public bool Repopulate()
+    {
+        // Destroy all that are not destroyed
+        for (int i = 0; i < last; i++)
+        {
+            Destroy(stack[i]);
+        }
+        
+        last = 5;
+        int layer = LayerMask.NameToLayer("stack");
+
         // Create new objects to stack
         for (int i = 0; i < 5; i++)
         {
             stack[i] = Instantiate(hex_cell,transform.position + new Vector3(0,i*1.25f, 3), Quaternion.Euler(-90+150,0,0));
             stack[i].layer = layer;
             stack[i].transform.parent = transform;
+            
+            var biomes = Enum.GetValues(typeof(Biome));
+            stack[i].GetComponent<Cell>().Biome = Enumerable.Repeat(0,1).Select(i =>
+            {
+                var rand = Random.Range(0, biomes.Length);
+                var obj = biomes.GetValue(rand);
+                return (Biome)obj; // gross
+            }).ToArray()[0];
         }
+
+
+        return true;
     }
+
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// You should use this like this
+    /// `
+    /// if(Pop(out var biome) ) { }
+    /// `
+    /// </summary>
+    /// <param name="biome"></param>
+    /// <returns></returns>
+    public bool Pop(out Biome biome)
+    {
+        if (last == 0)
+        {
+            biome = 0; // Garbage
+            return false;
+        }
+        var obj = stack[last-1];
+        biome = obj.GetComponent<Cell>().Biome;
+        last -= 1;
+        Destroy(obj);
+        return true;
     }
 }
